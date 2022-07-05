26f615c323e773b313c978017c3023831875bec7
chosokabe1 <tktkshi101110@gmail.com>
2022-07-06 00:15:42 +0900

    try_except_name

--- try_except_name.py
+++ try_except_name.py
@@ -1,3 +1,16 @@
 sum = 7600
 while True :
-    num = input("なん")
+    num = input("何人ですか?（qで終了）")
+    if num =="q":
+        print("終了しました")
+        break
+
+    try:
+        price = round(sum / int(num))
+        if price < 0:
+            continue
+        print("1人当たりの金額", price)
+    except ValueError:
+        print("数値を入れてください")
+    except ZeroDivisionError:
+        print("0以外の数値を入力してください")

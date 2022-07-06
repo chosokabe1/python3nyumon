40f0f5b463c35f9329333165a555bf4383bf820c
chosokabe1 <tktkshi101110@gmail.com>
2022-07-06 10:03:11 +0900

    inheritance

--- car_class2.py
+++ car_class2.py
@@ -1,5 +1,16 @@
 class Car:
+    #クラス変数
+    maker = "PEACE" #自動車メーカ
+    count = 0 #台数
+
+    @classmethod
+    def countup(cls):
+        cls.count += 1
+        print(f"出荷台数:{cls.count}")
+
     def __init__(self, color = "white"):
+        Car.countup()
+        self.mynumber = Car.count
         self.color = color
         self.mileage = 0
     
